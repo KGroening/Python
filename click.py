bc45ab6c5785cb006628408fd184f1ff670bf7f8
KGroening <53449593+KGroening@users.noreply.github.com>
2019-10-22 13:32:41 -0500

    Add files via upload
    Call answer

---
+++ click.py
@@ -0,0 +1,12 @@
+#! python3
+import pyautogui
+import time
+
+pyautogui.moveTo(80, 1004)
+pyautogui.click(button='left')
+time.sleep(0.5)
+pyautogui.moveTo(97, 961)
+pyautogui.click(button='left')
+time.sleep(0.5)
+pyautogui.moveTo(150, 114)
+pyautogui.click(button='left')
